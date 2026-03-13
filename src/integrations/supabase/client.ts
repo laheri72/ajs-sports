5e83881afee0488ccff1352fde2b61836f7305b8
laheri72 <idrislaheri72@gmail.com>
2026-03-13 15:00:24 +0530

    session url

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,13 +5,11 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
-
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
+    detectSessionInUrl: true
   }
 });
